d4b2dcb5d936be3d37771e6471a948d404334def
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-10-05 18:10:50 +0100

    Dodecahedron rotates

--- GE1 Examples 2022/Assets/RotateMe.cs
+++ GE1 Examples 2022/Assets/RotateMe.cs
@@ -1,19 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RotateMe : MonoBehaviour
 {
     [Range(0, 360)]
     public float speed = 90;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
     }
 }
