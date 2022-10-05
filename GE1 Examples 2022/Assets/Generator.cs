713dfb6151259644e5fe5ac540a665484f0d410a
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-10-05 21:55:22 +0100

    Created multiple circles using a while loop

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -6,27 +6,43 @@ public class Generator : MonoBehaviour
 {
     public int loops = 10;
     public GameObject prefab;
-    public int numberofDodecahedrons = 6;
-    
+    public int circumference;
+
     public float radius;
-    private float sin;
-    private float cos;
+    private float x;
+    private float z;
     private float angle;
     private Vector3 position;
     private Quaternion rotation;
+    private float cx;
+    private float cz;
+    private int j;
 
-    // Update is called once per frame
-    void Start()
+    private GameObject dodecahedron;
+
+    private void Update()
     {
-        for (int i = 0; i < numberofDodecahedrons; i++)
-        {
-            angle = i * Mathf.PI * 2 / numberofDodecahedrons;
-            cos = Mathf.Cos(angle) * radius;
-            sin = Mathf.Sin(angle) * radius;
-            position = transform.position + new Vector3(cos, 0, sin);
-            rotation = Quaternion.Euler(0, angle, 0);
-            Instantiate(prefab, position, rotation);
-        }
 
+        while (j < loops)
+        {
+            j++;
+            radius += 1f;
+            circumference = (int) (2 * Mathf.PI * radius);
+
+            for (int i = 0; i < circumference; i++)
+            {
+                angle = i * Mathf.PI * 2 / circumference;
+
+                cx = transform.position.x;
+                cz = transform.position.z;
+                x = cx + Mathf.Cos(angle) * radius;
+                z = cz + Mathf.Sin(angle) * radius;
+                //position = transform.position + new Vector3(x, 0, z);
+                //rotation = Quaternion.Euler(0, angle, 0);
+                dodecahedron = Instantiate(prefab);
+                dodecahedron.transform.position = new Vector3(x, 0, z);
+            }
+        }
     }
 }
+
