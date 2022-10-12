5b0247bc5c0f1e035b68d3340228d46f1c761231
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-10-12 16:12:58 +0100

    The cubes spawn in & child themselves on Start

--- GE1 Examples 2022/Assets/Fish.cs
+++ GE1 Examples 2022/Assets/Fish.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject fishBody;
+    private GameObject fishHead;
+    private GameObject fishTail;
 
-    // Update is called once per frame
-    void Update()
+    private Transform fish;
+    public Transform head;
+    public Transform tail;
+
+    private Material bodyMat;
+    private Material headMat;
+    private Material tailMat;
+
+    private void Start()
     {
+        //assigning the parents
+        fish = FindObjectOfType<Fish>().transform;
+        head = GameObject.Find("Head").transform;
+        tail = GameObject.Find("Tail").transform;
         
+        //creating the cubes
+        fishBody = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        fishHead = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        fishTail = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        //making them grey
+        bodyMat = fishBody.GetComponent<MeshRenderer>().material;
+        headMat = fishHead.GetComponent<MeshRenderer>().material;
+        tailMat = fishTail.GetComponent<MeshRenderer>().material;
+        bodyMat.color = Color.gray;
+        headMat.color = Color.gray;
+        tailMat.color = Color.gray;
+
+        //changing cubes' scale
+        fishBody.transform.localScale = new Vector3(2, 1, 1);
+        fishHead.transform.localScale = new Vector3(2, 1, 1);
+        fishTail.transform.localScale = new Vector3(2, 1, 1);
+        
+        
+        //setting the parents
+        fishBody.transform.SetParent(fish, false);
+        fishHead.transform.SetParent(head, false);
+        fishTail.transform.SetParent(tail, false);
     }
 }
