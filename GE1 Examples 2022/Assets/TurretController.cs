487ba3a4b51b2eafe87783c572c2c1520d312623
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2022-11-16 20:05:55 +0000

    (IN LAB7 SCENE) Made turret rotate with quaternions

--- GE1 Examples 2022/Assets/TurretController.cs
+++ GE1 Examples 2022/Assets/TurretController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,24 +13,35 @@ public class TurretController : MonoBehaviour
     public float rotationSpeed;
     public float fireRate;
     private GameObject tankBullet;
-    private bool coroutineStarted;
     private IEnumerator coroutine;
+    private bool playerInRange;
+    private Transform player;
+    private Quaternion rotation;
+    private Vector3 position;
 
     private void Start()
     {
         turretFOV = GameObject.Find("ColliderCheck").GetComponent<Collider>();
         turretFOV.isTrigger = true;
         spawnPoint = GameObject.Find("SpawnPoint").transform;
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            if (coroutine != null) return;
-            coroutine = shootPlayer();
-            StartCoroutine(coroutine);
-        }
+        if (!other.CompareTag("Player")) return;
+        if (coroutine != null) return;
+        coroutine = shootPlayer();
+        StartCoroutine(coroutine);
+        playerInRange = true;
     }
 
     private void OnTriggerExit(Collider other)
@@ -37,6 +49,7 @@ public class TurretController : MonoBehaviour
         if (coroutine == null) return;
         StopCoroutine(coroutine);
         coroutine = null;
+        playerInRange = false;
     }
 
     private IEnumerator shootPlayer()
@@ -48,5 +61,13 @@ public class TurretController : MonoBehaviour
             tankBullet.transform.position = spawnPoint.position;
             yield return new WaitForSeconds(1 / fireRate);
         }
+    }
+
+    private void Update()
+    {
+        if (!playerInRange) return;
+        position = player.position - transform.position;
+        rotation = Quaternion.LookRotation(position, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
